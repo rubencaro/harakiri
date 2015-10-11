ef2922ecc1ac1b54088b9d4bfe74f643095d7723
Rubén Caro <ruben@elpulgardelpanda.com>
2015-10-11 20:39:18 +0200

    Improved spit

--- lib/helpers.ex
+++ lib/helpers.ex
@@ -17,14 +17,23 @@ defmodule Harakiri.Helpers do
   def env(app, key, default), do: Application.get_env(app, key, default)
 
   @doc """
-    Spit to logger any passed variable, with location information.
+    Spit to output any passed variable, with location information.
   """
-  defmacro spit(obj, inspect_opts \\ []) do
+  defmacro spit(obj \\ "", inspect_opts \\ []) do
     quote do
       %{file: file, line: line} = __ENV__
-      [ :bright, :red, "\n\n#{file}:#{line}",
-        :normal, "\n\n#{inspect(unquote(obj),unquote(inspect_opts))}\n\n", :reset]
-      |> IO.ANSI.format(true) |> IO.puts
+      name = Process.info(self)[:registered_name]
+      chain = [ :bright, :red, "\n\n#{file}:#{line}",
+                :normal, "\n     #{inspect self}", :green," #{name}"]
+
+      msg = inspect(unquote(obj),unquote(inspect_opts))
+      if String.length(msg) > 2, do: chain = chain ++ [:red, "\n\n#{msg}"]
+
+      # chain = chain ++ [:yellow, "\n\n#{inspect Process.info(self)}"]
+
+      (chain ++ ["\n\n", :reset]) |> IO.ANSI.format(true) |> IO.puts
+
+      unquote(obj)
     end
   end
 
