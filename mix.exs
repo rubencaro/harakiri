a8e0990b8fbbec5611dc57d9d344b38d9f46390f
Rubén Caro <ruben.caro.estevez@gmail.com>
2017-07-30 17:18:40 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Harakiri.Mixfile do
 
   def project do
     [app: :harakiri,
-     version: "1.1.1",
+     version: "1.2.0",
      elixir: ">= 1.0.0",
      package: package(),
      description: """
